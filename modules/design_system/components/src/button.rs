ebdd7c1607c49776b46c579b54bd965178bd62fa
domsteinbach <dominique.steinbach@unibas.ch>
2025-11-04 11:43:46 +0100

    style: outsource common tw-classes

--- modules/design_system/components/src/button.rs
+++ modules/design_system/components/src/button.rs
@@ -1,17 +1,19 @@
 // TODO: find a solution for button-style anchor tags
 use maud::{html, Markup};
 
+const BASE_CLASSES: &str =
+    "rounded-md px-3 py-2 text-sm font-semibold shadow-xs cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed";
+
 #[derive(Debug, Clone)]
 pub enum ButtonVariant {
     Primary,
     Secondary,
 }
-
 impl ButtonVariant {
-    fn css_classes(&self) -> &'static str {
+    fn variant_classes(&self) -> &'static str {
         match self {
-            ButtonVariant::Primary => "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500",
-            ButtonVariant::Secondary => "rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20",
+            ButtonVariant::Primary => "bg-indigo-600 text-white hover:bg-indigo-300 focus-visible:outline-2 dark:bg-indigo-500 dark:text-white dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500",
+            ButtonVariant::Secondary => "bg-indigo-300 text-indigo-900 hover:bg-indigo-600 focus-visible:outline-2",
         }
     }
 
@@ -41,7 +43,7 @@ pub fn button_with_variant_and_testid(
     let test_id = custom_test_id.unwrap_or(variant.test_id());
 
     html! {
-        button type="button" class=(variant.css_classes()) disabled[disabled] data-testid=(test_id) {
+        button type="button" class=(format!("{} {}", BASE_CLASSES, variant.variant_classes())) disabled[disabled] data-testid=(test_id) {
             (text)
         }
     }

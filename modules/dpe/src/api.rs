128c95d24e25e9e9208159ca8d688fd13af52d8a
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-01-15 12:51:16 +0100

    chore: use /api/v2 prefix for json routes (DEV-5769)

--- modules/dpe/src/api.rs
+++ modules/dpe/src/api.rs
@@ -16,7 +16,7 @@ pub fn routes() -> Router<Arc<AppState>> {
     Router::new()
         .route("/projects", get(project::projects_index_handler))
         .route("/projects/{id}", get(project::project_show_handler))
-        .route("/projects/{id}/json", get(project::project_show_json_handler))
+        .route("/api/v2/projects/{id}", get(project::project_show_json_handler))
         .route("/", get(home_redirect_to_project))
         .nest_service("/assets", serve_dir.clone())
 }

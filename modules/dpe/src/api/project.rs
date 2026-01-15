b0e819595b58c37a8e94bbbe14a61ad0018c1b0b
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-01-15 12:50:10 +0100

    feat: make project list the default, create project show view (DEV-5769) (#70)
    * feat: make project list the default, create project show view (DEV-5769)
    
    * clippy warning

--- modules/dpe/src/api/project.rs
+++ modules/dpe/src/api/project.rs
@@ -1,12 +1,32 @@
 use std::sync::Arc;
 
+use askama::Template;
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::response::Html;
 
 use crate::app_state::AppState;
+use crate::domain::project::Project;
 use crate::domain::shortcode::Shortcode;
 use crate::error::ServerError;
+use crate::views::project::{ProjectIndexTemplate, ProjectShowTemplate};
+
+/// GET / — returns the homepage
+pub(crate) async fn projects_index_handler(State(state): State<Arc<AppState>>) -> Result<Html<String>, ServerError> {
+    let projects: Vec<Project> = state
+        .project_repository
+        .list()
+        .into_iter()
+        .take(12)
+        .filter_map(|p| {
+            let sc = state.project_repository.find_shortcode(p);
+            sc.and_then(|sc| state.project_repository.read(sc))
+        })
+        .collect::<Vec<Project>>();
+
+    let view = (ProjectIndexTemplate { projects }).render().unwrap();
+    Ok(Html(view))
+}
 
 fn return_ok_or_404<B>(body: Option<B>, otherwise: B) -> (StatusCode, B) {
     match body {
@@ -15,8 +35,19 @@ fn return_ok_or_404<B>(body: Option<B>, otherwise: B) -> (StatusCode, B) {
     }
 }
 
-// GET /v2/projects/:id.json — returns a single project as HTML
-pub(crate) async fn project_json_handler(
+// GET /v2/projects/:id — returns a single project as HTML
+pub(crate) async fn project_show_handler(
+    Path(shortcode): Path<Shortcode>,
+    State(state): State<Arc<AppState>>,
+) -> Result<(StatusCode, Html<String>), ServerError> {
+    let project: Option<Project> = state.project_repository.read(shortcode);
+
+    let view: Option<String> = project.map(|project| ProjectShowTemplate { project }.render().unwrap());
+    Ok(return_ok_or_404(view.map(Html), Html("missing".to_string())))
+}
+
+// GET /v2/projects/:id.json — returns a single project as JSON
+pub(crate) async fn project_show_json_handler(
     Path(shortcode): Path<Shortcode>,
     State(state): State<Arc<AppState>>,
 ) -> Result<(StatusCode, Html<String>), ServerError> {
--- modules/dpe/src/views/home.rs
+++
@@ -1,7 +0,0 @@
-use crate::domain::project::Project;
-
-#[derive(askama::Template)]
-#[template(path = "home/index.html")]
-pub(crate) struct IndexTemplate {
-    pub projects: Vec<Project>,
-}

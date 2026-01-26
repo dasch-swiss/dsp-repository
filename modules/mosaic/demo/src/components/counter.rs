a6b74b10ece9831282d40545f36d5bb2835868db
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-01-26 13:43:58 +0100

    styling counter

--- modules/mosaic/demo/src/components/counter.rs
+++ modules/mosaic/demo/src/components/counter.rs
@@ -1,14 +1,21 @@
 use leptos::prelude::*;
-use mosaic_tiles::{button::Button, button_group::ButtonGroup};
+use mosaic_tiles::{
+    button::{Button, ButtonVariant},
+    button_group::ButtonGroup,
+};
 
 #[component]
 pub fn Counter() -> impl IntoView {
     let (count, set_count) = signal(0);
     view! {
         <ButtonGroup>
-            <Button on:click=move |_| set_count.update(|n| *n -= 1)>"-"</Button>
-            <Button disabled=true>{count}</Button>
-            <Button on:click=move |_| set_count.update(|n| *n += 1)>"+"</Button>
+            <Button variant=ButtonVariant::Outline on:click=move |_| set_count.update(|n| *n -= 1)>
+                "-"
+            </Button>
+            <Button variant=ButtonVariant::Outline>{count}</Button>
+            <Button variant=ButtonVariant::Outline on:click=move |_| set_count.update(|n| *n += 1)>
+                "+"
+            </Button>
         </ButtonGroup>
     }
 }

5668e9fcc28e59b21edfb802ef9a41db5f18e014
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-01-26 13:54:09 +0100

    rename selection to interactive

--- modules/mosaic/demo/src/components/button_group/examples/mod.rs
+++ modules/mosaic/demo/src/components/button_group/examples/mod.rs
@@ -1,11 +1,11 @@
 pub mod basic;
 pub mod disabled;
-pub mod selection;
+pub mod interactive;
 pub mod sizes;
 pub mod with_icons;
 
 pub use basic::*;
 pub use disabled::*;
-pub use selection::*;
+pub use interactive::*;
 pub use sizes::*;
 pub use with_icons::*;

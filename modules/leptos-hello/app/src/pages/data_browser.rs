1635a71fc5e60097df510a1c0fb4948510eca661
Julien Schneider <julien.schneider.1991@gmail.com>
2026-01-28 14:38:01 +0100

    wip: solution client side not working

--- modules/leptos-hello/app/src/pages/data_browser.rs
+++ modules/leptos-hello/app/src/pages/data_browser.rs
@@ -1,5 +1,7 @@
+use leptos::leptos_dom::log;
 use leptos::prelude::*;
 use leptos_router::hooks::use_params_map;
+use web_sys::{MouseEvent};
 
 #[component]
 pub fn DataBrowserPage() -> impl IntoView {
@@ -11,24 +13,50 @@ pub fn DataBrowserPage() -> impl IntoView {
     view! {
         <h1 class="text-2xl mb-4">Data Browser</h1>
         <div class="flex">
-            <div style="height: 70vh; overflow: auto">
-                {elements.into_iter().enumerate().map(|(i, element)| {
-                    let id = i + 1;
-                    view!{
-                        <a href=format!("/data-browser/element-{}", id)>
-                            <div class="cursor-pointer hover:bg-gray-100 p-2">
-                                {element}
-                            </div>
-                        </a>
-                    }
-                }).collect_view()}
-            </div>
+            <DataBrowserList elements=elements />
 
             {move || selected_id().map(|id| view! {
                 <div class="p-4 flex-1" style="background-color: #aae5ea;">
                     <h2>"Resource view for " {id}</h2>
                 </div>
             })}
+        </div>
+    }
+}
+
+#[island]
+fn DataBrowserList(elements: Vec<String>) -> impl IntoView {
+    let navigate_to = move |url: String| {
+        log!("navigate to {}", url);
+        if let Some(window) = web_sys::window() {
+            if let Some(history) = window.history().ok() {
+                let _ = history.push_state_with_url(&wasm_bindgen::JsValue::NULL, "", Some(&url));
+                // Trigger a popstate event to update the UI
+                if let Some(event) = web_sys::PopStateEvent::new("popstate").ok() {
+                    let _ = window.dispatch_event(&event);
+                }
+            }
+        }
+    };
+
+    view! {
+        <div style="height: 70vh; overflow: auto">
+            {elements.into_iter().enumerate().map(|(i, element)| {
+                let id = i + 1;
+                let url = format!("/data-browser/element-{}", id);
+                view!{
+                    <div
+                        on:click=move |e: MouseEvent| {
+                            e.prevent_default();
+                            navigate_to(url.clone());
+                        }
+                    >
+                        <div class="cursor-pointer hover:bg-gray-100 p-2">
+                            {element}
+                        </div>
+                    </div>
+                }
+            }).collect_view()}
         </div>
     }
 }

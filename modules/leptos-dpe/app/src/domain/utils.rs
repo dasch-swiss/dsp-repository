aed02ca00d0b99a0bc007b845b412d59e6024c5e
Julien Schneider <julien.schneider.1991@gmail.com>
2026-02-09 11:13:11 +0100

    rename leptos-hello to leptos-dpe

--- modules/leptos-dpe/app/src/domain/utils.rs
+++ modules/leptos-dpe/app/src/domain/utils.rs
@@ -7,5 +7,5 @@ pub fn get_data_dir() -> String {
 
     // For development with cargo-leptos, use relative path from workspace root
     // cargo-leptos runs from the workspace root
-    "modules/leptos-hello/server/data".to_string()
+    "modules/leptos-dpe/server/data".to_string()
 }

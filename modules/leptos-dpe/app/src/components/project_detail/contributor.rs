1b5de0a91bc49db935479654a7ee5d9be2cd7bbe
Julien Schneider <julien.schneider.1991@gmail.com>
2026-02-24 14:34:33 +0100

    wip

--- modules/leptos-dpe/app/src/components/project_detail/contributor.rs
+++ modules/leptos-dpe/app/src/components/project_detail/contributor.rs
@@ -1,15 +1,15 @@
 use leptos::prelude::*;
 
-use crate::components::Person;
+use crate::components::{InfoCard, Person};
 use crate::domain::Attribution;
 
 #[component]
 pub fn Contributor(attr: Attribution) -> impl IntoView {
     let person_id = attr.contributor.clone();
     view! {
-        <div class="border-l-4 border-primary pl-4">
+        <InfoCard>
             <div class="text-xs text-base-content/60 mb-2">{attr.contributor_type.join(", ")}</div>
             <Person person_id=person_id />
-        </div>
+        </InfoCard>
     }
 }

4fa49b87e86b3d4a71e1a7023fd2a423b6c9cb7b
Julien Schneider <julien.schneider.1991@gmail.com>
2026-02-25 14:10:49 +0100

    wip

--- modules/leptos-dpe/app/src/pages/project/components/breadcrumb.rs
+++ modules/leptos-dpe/app/src/pages/project/components/breadcrumb.rs
@@ -1,9 +1,7 @@
 use leptos::prelude::*;
-use mosaic_tiles::icon::{Icon, IconChevronRight};
 
 #[component]
 pub fn Breadcrumb(project_name: String) -> impl IntoView {
-    // Truncate project name to 50 characters with ellipsis
     let project_name_truncated = if project_name.len() > 100 {
         format!("{}...", &project_name[..50])
     } else {
@@ -11,22 +9,12 @@ pub fn Breadcrumb(project_name: String) -> impl IntoView {
     };
 
     view! {
-        <nav class="flex items-center gap-2 text-sm text-gray-600">
-            <a href="/">
-                "Home"
-            </a>
-            <span class="text-gray-400">
-                <Icon icon=IconChevronRight class="w-3 h-3" />
-             </span>
-
-            <a href="/projects" class="text-primary-400">
-                "Projects"
-            </a>
-            <span class="text-gray-400">
-                <Icon icon=IconChevronRight class="w-3 h-3" />
-             </span>            <span class="text-gray-900 font-medium truncate">
-                {project_name_truncated}
-            </span>
-        </nav>
+        <div class="breadcrumbs text-sm">
+            <ul>
+                <li><a href="/">"Home"</a></li>
+                <li><a href="/projects">"Projects"</a></li>
+                <li>{project_name_truncated}</li>
+            </ul>
+        </div>
     }
 }

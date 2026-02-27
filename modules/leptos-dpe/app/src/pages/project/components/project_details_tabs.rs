6739f8128ead162093b2c45c96edbf2a7cad47df
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-02-27 13:57:12 +0100

    replace tabs

--- modules/leptos-dpe/app/src/pages/project/components/project_details_tabs.rs
+++ modules/leptos-dpe/app/src/pages/project/components/project_details_tabs.rs
@@ -1,6 +1,7 @@
 use leptos::prelude::*;
-use mosaic_tiles::card::{Card, CardBody, CardVariant};
-use mosaic_tiles::icon::{Data, Document, Icon, Info, People};
+use mosaic_tiles::card::{Card, CardVariant};
+use mosaic_tiles::icon::{Data, Document, Info, People};
+use mosaic_tiles::tabs::{Tab, Tabs};
 
 use crate::domain::{Attribution, Project};
 use crate::pages::project::components::attributions_section::AttributionsSection;
@@ -12,53 +13,28 @@ pub fn ProjectDetailsTabs(proj: Project, attributions: Vec<Attribution>) -> impl
     let abstract_en = proj.abstract_text.as_ref().and_then(|m| m.get("en").cloned());
     let publications = proj.publications.clone();
 
-    let tab_label_class = "tab gap-1 px-4 pb-2";
-    let tab_content_class = "border-base-300 tab-content rounded-none border-0 border-t p-6";
-    let icon_class = "h-6 text-neutral-400";
-
     view! {
-        <Card variant=CardVariant::Bordered class="flex-1">
-            <CardBody>
-                <div class="tabs tabs-border pt-4">
-                    <label class=tab_label_class>
-                        <Icon icon=Info class=icon_class />
-
-                        <input type="radio" name="my_tabs" checked="checked" />
-                        Dataset Overview
-                    </label>
-                    <div class=tab_content_class>
-                        <DatasetOverviewSection proj=proj />
-                    </div>
-
-                    <label class=tab_label_class>
-                        <Icon icon=Data class=icon_class />
-
-                        <input type="radio" name="my_tabs" />
-                        Data
-                    </label>
-                    <div class=tab_content_class>TODO</div>
-
-                    <label class=tab_label_class>
-                        <Icon icon=Document class=icon_class />
-
-                        <input type="radio" name="my_tabs" />
-                        Publications
-                    </label>
-                    <div class=tab_content_class>
-                        <PublicationTab abstract_en=abstract_en publications=publications />
-                    </div>
-
-                    <label class=tab_label_class>
-                        <Icon icon=People class=icon_class />
-
-                        <input type="radio" name="my_tabs" />
-                        Contributors
-                    </label>
-                    <div class=tab_content_class>
-                        <AttributionsSection attributions=attributions />
-                    </div>
-                </div>
-            </CardBody>
+        <Card variant=CardVariant::Bordered class="flex-1 pt-4">
+            <Tabs>
+                <Tab
+                    name="project-tabs"
+                    value="overview"
+                    label="Dataset Overview"
+                    icon=Info
+                    checked=true
+                >
+                    <DatasetOverviewSection proj=proj />
+                </Tab>
+                <Tab name="project-tabs" value="data" label="Data" icon=Data>
+                    "TODO"
+                </Tab>
+                <Tab name="project-tabs" value="publications" label="Publications" icon=Document>
+                    <PublicationTab abstract_en=abstract_en publications=publications />
+                </Tab>
+                <Tab name="project-tabs" value="contributors" label="Contributors" icon=People>
+                    <AttributionsSection attributions=attributions />
+                </Tab>
+            </Tabs>
         </Card>
     }
 }

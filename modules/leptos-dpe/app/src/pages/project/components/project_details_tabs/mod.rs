cf343d1c19e7e38bc6fcaaa221b9083d064ef161
Julien Schneider <julien.schneider.1991@gmail.com>
2026-03-13 11:17:28 +0100

    wip: routing for tab

--- modules/leptos-dpe/app/src/pages/project/components/project_details_tabs/mod.rs
+++ modules/leptos-dpe/app/src/pages/project/components/project_details_tabs/mod.rs
@@ -3,8 +3,9 @@ mod dataset_overview_section;
 mod publication_tab;
 
 use leptos::prelude::*;
+use leptos_router::hooks::use_query_map;
 use mosaic_tiles::icon::{Document, Info, People};
-use mosaic_tiles::tabs::{Tab, Tabs};
+use mosaic_tiles::icon::IconData;
 
 use crate::domain::{lang_value, Project, ResolvedContributor};
 use attributions_section::AttributionsSection;
@@ -17,35 +18,79 @@ pub fn ProjectDetailsTabs(proj: Project, contributors: Vec<ResolvedContributor>)
     let publications = proj.publications.clone();
     let has_publications_tab = abstract_en.is_some() || publications.as_ref().map(|p| !p.is_empty()).unwrap_or(false);
 
+    let query = use_query_map();
+    let active_tab = query.read().get("tab").unwrap_or_else(|| "overview".to_string());
+
     view! {
         <div class="dpe-card flex-1 pt-4">
-            <Tabs>
-                <Tab
-                    name="project-tabs"
+            <div class="tabs" style="border-width: 0">
+                <TabLink
                     value="overview"
-                    label="Dataset Overview"
+                    active_tab=active_tab.clone()
                     icon=Info
-                    checked=true
-                >
-                    <DatasetOverviewSection proj=proj />
-                </Tab>
+                    label="Dataset Overview"
+                />
                 {has_publications_tab
                     .then(|| {
                         view! {
-                            <Tab
-                                name="project-tabs"
+                            <TabLink
                                 value="publications"
-                                label="Publications"
+                                active_tab=active_tab.clone()
                                 icon=Document
-                            >
-                                <PublicationTab abstract_en=abstract_en publications=publications />
-                            </Tab>
+                                label="Publications"
+                            />
                         }
                     })}
-                <Tab name="project-tabs" value="contributors" label="Contributors" icon=People>
-                    <AttributionsSection contributors=contributors />
-                </Tab>
-            </Tabs>
+                <TabLink
+                    value="contributors"
+                    active_tab=active_tab.clone()
+                    icon=People
+                    label="Contributors"
+                />
+
+                <div class="tab-panel" style="display: block">
+                    {match active_tab.as_str() {
+                        "publications" if has_publications_tab => {
+                            view! {
+                                <PublicationTab abstract_en=abstract_en publications=publications />
+                            }
+                                .into_any()
+                        }
+                        "contributors" => {
+                            view! { <AttributionsSection contributors=contributors /> }.into_any()
+                        }
+                        _ => view! { <DatasetOverviewSection proj=proj /> }.into_any(),
+                    }}
+                </div>
+            </div>
         </div>
+    }
+}
+
+#[component]
+fn TabLink(
+    value: &'static str,
+    active_tab: String,
+    icon: IconData,
+    label: &'static str,
+) -> impl IntoView {
+    let is_active = active_tab == value;
+    let class = if is_active {
+        "tab-label !text-primary-600 !border-primary-600"
+    } else {
+        "tab-label"
+    };
+
+    view! {
+        <a href=format!("?tab={value}") class=class>
+            <svg
+                class="tab-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox=icon.view_box
+                fill="currentColor"
+                inner_html=icon.data
+            ></svg>
+            <span>{label}</span>
+        </a>
     }
 }
